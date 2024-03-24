de6c2befe8e426f5156ab1adad20ac43dcc4b6a5
elliod <r4t00b1@gmail.com>
2024-03-24 00:28:00 -0300

    style(cooperative): fix text position

--- packages/nextjs/app/cooperative/[id]/page.tsx
+++ packages/nextjs/app/cooperative/[id]/page.tsx
@@ -60,39 +60,44 @@ export default function Cooperative({ params }: { params: { id: string } }) {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-14">
-      <div className="aspect-[1.75] relative [&>p]:text-yellow-950">
-        <Image src={cooperative.image} alt={cooperative.name} width={517} height={295} layout="responsive" />
-        <span className="z-50 text-yellow-50 absolute bottom-4 left-5 font-semibold text-[32px]">
-          {cooperative.name}
-        </span>
+    <article className="grid grid-cols-2 gap-14">
+      <section>
+        <div className="aspect-[1.75] relative [&>p]:text-yellow-950">
+          <Image src={cooperative.image} alt={cooperative.name} width={517} height={295} layout="responsive" />
+          <span className="z-50 text-yellow-50 absolute bottom-6 left-8 font-semibold text-[32px]">
+            {cooperative.name}
+          </span>
+        </div>
 
-        <p>Cooperativa Soja Verde: Cultivando o Futuro com Você</p>
-        <p>
-          A Cooperativa Soja Verde é a união de pequenos produtores que acreditam na força da união e do trabalho
-          conjunto para construir um futuro melhor para o agronegócio brasileiro. Nascida da paixão pela terra e pelo
-          cultivo da soja, nossa cooperativa se dedica a fornecer produtos de alta qualidade para o mercado, assegurando
-          preços justos para os produtores e promovendo o desenvolvimento social e econômico das comunidades onde
-          atuamos.
-        </p>
+        <div>
+          <p>Cooperativa Soja Verde: Cultivando o Futuro com Você</p>
+          <p>
+            A Cooperativa Soja Verde é a união de pequenos produtores que acreditam na força da união e do trabalho
+            conjunto para construir um futuro melhor para o agronegócio brasileiro. Nascida da paixão pela terra e pelo
+            cultivo da soja, nossa cooperativa se dedica a fornecer produtos de alta qualidade para o mercado,
+            assegurando preços justos para os produtores e promovendo o desenvolvimento social e econômico das
+            comunidades onde atuamos.
+          </p>
 
-        <p>
-          Com um compromisso inabalável com a sustentabilidade, empregamos práticas agrícolas inovadoras que preservam o
-          meio ambiente e garantem a qualidade da nossa soja. Através da constante busca por conhecimento e tecnologia,
-          investimos na capacitação dos nossos cooperados e na modernização da produção, impulsionando a competitividade
-          do nosso setor.
-        </p>
+          <p>
+            Com um compromisso inabalável com a sustentabilidade, empregamos práticas agrícolas inovadoras que preservam
+            o meio ambiente e garantem a qualidade da nossa soja. Através da constante busca por conhecimento e
+            tecnologia, investimos na capacitação dos nossos cooperados e na modernização da produção, impulsionando a
+            competitividade do nosso setor.
+          </p>
 
-        <p>
-          Ao investir na Cooperativa Soja Verde, você estará investindo em um futuro mais próspero para o agronegócio
-          brasileiro. Você estará contribuindo para a produção de alimentos saudáveis e de alta qualidade, para a
-          sustentabilidade da agricultura familiar e para o desenvolvimento social e econômico das comunidades rurais.
-        </p>
+          <p>
+            Ao investir na Cooperativa Soja Verde, você estará investindo em um futuro mais próspero para o agronegócio
+            brasileiro. Você estará contribuindo para a produção de alimentos saudáveis e de alta qualidade, para a
+            sustentabilidade da agricultura familiar e para o desenvolvimento social e econômico das comunidades rurais.
+          </p>
 
-        <p>Junte-se a nós e faça parte dessa história de sucesso!</p>
-      </div>
+          <p>Junte-se a nós e faça parte dessa história de sucesso!</p>
+          {/* </div> */}
+        </div>
+      </section>
 
-      <div className="shadow-xl rounded-[10px] px-14 pt-8 pb-14 shadow-yellow-900/20 flex flex-col bg-yellow-50">
+      <section className="shadow-xl rounded-[10px] px-14 pt-8 pb-14 shadow-yellow-900/20 flex flex-col bg-yellow-50">
         <Bar progress={cooperative.totalProgress} />
 
         <div className="flex justify-between [&>span]:font-semibold [&>span]:text-[38px] mt-5 [&>span]:text-yellow-950">
@@ -136,7 +141,7 @@ export default function Cooperative({ params }: { params: { id: string } }) {
         </div>
 
         <PurchaseTokens />
-      </div>
-    </div>
+      </section>
+    </article>
   );
 }
