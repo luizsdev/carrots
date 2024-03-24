256e6ea669bc822d318a11880ccb35357605a54b
elliod <r4t00b1@gmail.com>
2024-03-24 01:13:52 -0300

    chore(rewards): add claim tokens mock

--- packages/nextjs/app/rewards/page.tsx
+++ packages/nextjs/app/rewards/page.tsx
@@ -1,13 +1,22 @@
+"use client";
+
+import { useState } from "react";
 import { RewardCard } from "~~/components/rewards/RewardCard";
 
 export default function Rewards() {
+  const [isClaimed, setIsClaimed] = useState(false);
+
+  function handleClaimTokens() {
+    setIsClaimed(true);
+  }
+
   return (
     <div className="flex flex-col w-full gap-[50px]">
-      <RewardCard progress={50} />
-      <RewardCard progress={100} />
-      <RewardCard />
-      <RewardCard />
-      <RewardCard />
+      <RewardCard progress={100} disabled={isClaimed} onClaim={() => handleClaimTokens()} />
+      <RewardCard progress={50} disabled={true} onClaim={() => handleClaimTokens()} />
+      <RewardCard progress={70} disabled={true} onClaim={() => handleClaimTokens()} />
+      <RewardCard progress={20} disabled={true} onClaim={() => handleClaimTokens()} />
+      <RewardCard progress={10} disabled={true} onClaim={() => handleClaimTokens()} />
     </div>
   );
 }
--- packages/nextjs/components/rewards/RewardCard.tsx
+++ packages/nextjs/components/rewards/RewardCard.tsx
@@ -3,8 +3,10 @@ import { Bar } from "~~/app/marketplace/components/Bar";
 type RewardCardProps = {
   progress?: number;
   disabled?: boolean;
+  onClaim: () => void;
 };
-export function RewardCard({ progress = 100, disabled = false }: RewardCardProps) {
+
+export function RewardCard({ progress = 100, disabled = false, onClaim }: RewardCardProps) {
   return (
     <div className="w-full flex justify-between shadow-lg h-[134px] px-4 py-8 items-center text-yellow-950 text-2xl rounded-lg bg-yellow-50">
       <div className="flex gap-[150px] items-end h-full ">
@@ -24,6 +26,7 @@ export function RewardCard({ progress = 100, disabled = false }: RewardCardProps
           disabled={progress < 100 || disabled}
           className="bg-yellow-950 disabled:bg-opacity-50 disabled:hover:bg-yellow-950/50 disabled:cursor-not-allowed rounded-lg w-[192px] h-10 font-semibold text-lg text-yellow-50 hover:bg-yellow-900 transition-colors"
           type="button"
+          onClick={onClaim}
         >
           {disabled ? "Claimed" : "Claim Tokens"}
         </button>
